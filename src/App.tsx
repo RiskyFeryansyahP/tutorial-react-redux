5dd1ad7dd27b9372a4f514e2f90f9f6a0a309a50
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2018-12-17 23:59:36 +0700

    App component to wrapper Home Component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import Home from './components/Home'
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
